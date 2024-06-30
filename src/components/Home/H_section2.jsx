9155b1a2481d7a58504115eefd7846cb04f047b9
BP <bhagirathpaliyal@gmail.com>
2024-06-30 13:12:00 +0530

    Added Products Component For All Products

--- src/components/Home/H_section2.jsx
+++ src/components/Home/H_section2.jsx
@@ -19,7 +19,7 @@ function H_section2() {
   }, []);
 
   return (
-    <div className="bg-[#0055E9] flex flex-col gap-[50px] items-center justify-center text-white p-[50px]">
+    <div id="section2" className="bg-[#0055E9] flex flex-col gap-[50px] items-center justify-center text-white p-[50px]">
       <div className="text-[30px] md:text-[40px] ">Our collection</div>
       <div className="flex w-[100%] justify-evenly flex-wrap">
 
@@ -30,7 +30,7 @@ function H_section2() {
 {dataa?.map((data,index)=>
 
   (
-    <Link key={index} to={data==="men's clothing" ?"men":data ==="women's clothing"?"women":data}>
+    <Link key={index} to={data==="men's clothing" ?"/Products/Men":data ==="women's clothing"?"/Products/Women":`/Products/${data}`}>
 <div  className="max-w-[200px]  pb-[20px] flex flex-col items-center gap-[10px] ">
   <div className="w-[100%] rounded-[10px] overflow-hidden"><img src={man} alt="image" /></div>
   <div>
@@ -46,16 +46,11 @@ function H_section2() {
 
 
 
+ </div>
+
+<Link to={'/Products/AllProducts'}><h3>See All Products</h3></Link>
 
 
-       
-
-
-
-
-
-      
-      </div>
     </div>
   );
 }
